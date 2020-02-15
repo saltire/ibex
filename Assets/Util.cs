d924378a76f300a9f6e73ebf99e825f6e4e8ae97
saltire sable <saltiresable@gmail.com>
2020-02-15 15:06:53 -0500

    Ibex rough skeleton and walk movement

---
+++ Assets/Util.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Util {
+  public static void Log(params object[] items) {
+    string str = "";
+    foreach (object item in items) {
+      str += item;
+      str += " ";
+    }
+    Debug.Log(str);
+  }
+}

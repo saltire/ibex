b2e5ce98fd3a74b4b282e1a264dedf801edb46c2
saltire sable <saltiresable@gmail.com>
2020-02-16 18:22:13 -0500

    Walk the goat along the curve of the ground

--- Assets/Scripts/GroundScript.cs
+++ Assets/Scripts/GroundScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class GroundScript : MonoBehaviour {
 	public float width = 20;
-	public float pointCount = 20;
+	public int pointCount = 20;
 
 	public float centerHeight = 1;
 	public float edgeHeight = 5;
@@ -19,26 +19,28 @@ public class GroundScript : MonoBehaviour {
 		points.Add(new Vector2(-width / 2, 0));
 
 		for (int p = 0; p <= pointCount; p++) {
-			float x = p / pointCount - .5f;
+			float x = p / (float)pointCount - .5f;
 			points.Add(new Vector2(
 				transform.position.x + width * x,
 				centerHeight + (edgeHeight - centerHeight) * x * x));
 		}
 
+		GetComponent<PolygonCollider2D>().points = points.ToArray();
+
 		Triangulator tr = new Triangulator(points.ToArray());
 		int[] indices = tr.Triangulate();
 
 		Vector3[] vertices = new Vector3[points.Count];
-		for (int i = 0; i < vertices.Length; i++) {
+		for (int i = 0; i < points.Count; i++) {
 			vertices[i] = new Vector3(points[i].x, points[i].y, 0);
 		}
 
 		Mesh mesh = new Mesh();
+		mesh.name = "ground mesh";
 		mesh.vertices = vertices;
 		mesh.triangles = indices;
 		mesh.RecalculateNormals();
 		mesh.RecalculateBounds();
-
 		GetComponent<MeshFilter>().mesh = mesh;
 	}
 }
--- Assets/Scripts/MovementScript.cs
+++ Assets/Scripts/MovementScript.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class MovementScript : MonoBehaviour {
-	public Transform frontFeetMark;
-	public Transform backFeetMark;
-
 	public Transform[] frontFeet;
 	public Transform[] backFeet;
+	public Transform frontFeetMark;
+	public Transform backFeetMark;
+	public Collider2D ground;
 
 	public float speed = 0.05f;
+	public float feetDistance = 1f;
 	public float stepLength = 0.25f;
 
 	Vector2 inputVelocity;
@@ -20,15 +21,32 @@ public class MovementScript : MonoBehaviour {
 	}
 
 	void Update() {
-		transform.position += Vector3.right * inputVelocity.x * speed;
+		RaycastHit2D hit = Physics2D.Raycast(new Vector3(transform.position.x, 100, 0), Vector2.down);
+		transform.position = hit.point;
 
-		frontFeet[0].position = frontFeetMark.position + Vector3.right *
-			(-stepLength / 2 + Mathf.PingPong(transform.position.x, stepLength));
-		frontFeet[1].position = frontFeetMark.position + Vector3.right *
-			(stepLength / 2 - Mathf.PingPong(transform.position.x, stepLength));
-		backFeet[0].position = backFeetMark.position + Vector3.right *
-			(stepLength / 2 - Mathf.PingPong(transform.position.x, stepLength));
-		backFeet[1].position = backFeetMark.position + Vector3.right *
-			(-stepLength / 2 + Mathf.PingPong(transform.position.x, stepLength));
+		Vector3 angledRight = -Vector2.Perpendicular(hit.normal);
+		transform.rotation = Quaternion.Euler(0, 0, Vector2.SignedAngle(Vector2.right, angledRight));
+
+		frontFeetMark.position = ground.ClosestPoint(
+			transform.position + angledRight * feetDistance / 2);
+		backFeetMark.position = ground.ClosestPoint(
+			transform.position - angledRight * feetDistance / 2);
+
+		transform.position += angledRight * inputVelocity.x * speed;
+
+		PlaceFoot(frontFeet[0], frontFeetMark, false);
+		PlaceFoot(frontFeet[1], frontFeetMark, true);
+		PlaceFoot(backFeet[0], backFeetMark, false);
+		PlaceFoot(backFeet[1], backFeetMark, true);
+	}
+
+	void PlaceFoot(Transform foot, Transform midpoint, bool altFoot) {
+		float x = midpoint.position.x +
+			(Mathf.PingPong(transform.position.x, stepLength) - stepLength / 2) * (altFoot ? -1 : 1);
+
+		RaycastHit2D hit = Physics2D.Raycast(new Vector3(x, 100, 0), Vector2.down);
+		float y = hit.point.y;
+
+		foot.position = new Vector3(x, y, 0);
 	}
 }
